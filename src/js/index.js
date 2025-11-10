5cd19b7fc272b7e4e767b30dbe83750f3a8c8133
Vladimir Monteiro <vladimir.monteiro021@gmail.com>
2025-11-10 09:01:58 -0300

    Fix: ajustado filtro de produtos

--- src/js/index.js
+++ src/js/index.js
@@ -7,35 +7,7 @@ fetch("https://dummyjson.com/products")
     produtos = data.products;
     container = document.getElementById("produtos-card-container");
 
-    for (let i = 0; i < produtos.length; i++) {
-      const item = produtos[i];
-
-      container.innerHTML += `
-        <div class="card">
-          <img src="${item.thumbnail}" alt="${item.title}">
-          <h2>${item.title}</h2>
-          <p>${item.description}</p>
-          <div class="price">Preço: R$ ${item.price}</div>
-          <button class="btn-buy" data-id="${item.id}">Comprar</button>
-        </div>`;
-    }
-
-  
-    document.querySelectorAll(".btn-buy").forEach((btn) => {
-      btn.addEventListener("click", () => {
-        const id = btn.getAttribute("data-id");
-        const produtoSelecionado = produtos.find((p) => p.id == id);
-
-    
-        localStorage.setItem(
-          "produtoSelecionado",
-          JSON.stringify(produtoSelecionado)
-        );
-
-     
-        window.location.href = "product.html";
-      });
-    });
+    renderizarProdutos(produtos);
   })
   .catch((error) => {
     console.error("Erro ao carregar produto", error);
@@ -43,16 +15,47 @@ fetch("https://dummyjson.com/products")
       "<p>Erro ao carregar produtos</p>";
   });
 
+function renderizarProdutos(lista) {
+  container.innerHTML = "";
+
+  lista.forEach((item) => {
+    container.innerHTML += `
+      <div class="card">
+        <img src="${item.thumbnail}" alt="${item.title}">
+        <h2>${item.title}</h2>
+        <p>${item.description}</p>
+        <div class="price">Preço: R$ ${item.price}</div>
+        <button class="btn-buy" data-id="${item.id}">Comprar</button>
+      </div>`;
+  });
+
+  document.querySelectorAll(".btn-buy").forEach((btn) => {
+    btn.addEventListener("click", () => {
+      const id = btn.getAttribute("data-id");
+      const produtoSelecionado = produtos.find((p) => p.id == id);
+
+      localStorage.setItem(
+        "produtoSelecionado",
+        JSON.stringify(produtoSelecionado)
+      );
+
+      window.location.href = "product.html";
+    });
+  });
+}
+
 document.addEventListener("DOMContentLoaded", () => {
   const form = document.querySelector(".filterForm");
 
   form.addEventListener("submit", (e) => {
     e.preventDefault();
-    const inputValue = form.querySelector("input").value;
 
-    produtos = produtos.filter((p) => p.startWith(inputValue));
-    console.log(produtos);
+    const inputValue = form.querySelector("input").value.trim().toLowerCase();
 
-    alert(inputValue);
+    const filtrados = produtos.filter((p) =>
+      p.title.toLowerCase().startsWith(inputValue)
+    );
+
+    renderizarProdutos(filtrados);
   });
 });
